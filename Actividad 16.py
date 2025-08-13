324eca66c016ac1703d660fed625ec9c20dca9c6
Marvin291-dev <marvin.velasquez@udeo.edu.gt>
2025-08-13 10:53:06 -0600

    segundo commit

--- Actividad 16.py
+++ Actividad 16.py
@@ -1,6 +1,10 @@
 class Libro:
     def __init__(self, titulo, autor, año_Publicacion, Codigo):
+        self.Codigo = Codigo
         self.titulo = titulo
         self.autor = autor
         self.año_Publicacion = año_Publicacion
-        self.Codigo = Codigo
+
+
+    def mostrar_libro(self):
+        return f"Codigo: {self.Codigo} - titulo: {self.titulo} - autor: {self.autor} - año Publicación: {self.año_Publicacion}"
