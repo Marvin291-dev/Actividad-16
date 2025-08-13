9a4ca1101da9a6c7a0d8bf2ee4220dbaec17e0b5
Marvin291-dev <marvin.velasquez@udeo.edu.gt>
2025-08-13 10:46:25 -0600

    primer commit

---
+++ Actividad 16.py
@@ -0,0 +1,6 @@
+class Libro:
+    def __init__(self, titulo, autor, año_Publicacion, Codigo):
+        self.titulo = titulo
+        self.autor = autor
+        self.año_Publicacion = año_Publicacion
+        self.Codigo = Codigo
