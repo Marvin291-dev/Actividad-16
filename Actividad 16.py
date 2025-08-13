f53b34497561079f2cb6af9ae0315eca1b577580
Marvin291-dev <marvin.velasquez@udeo.edu.gt>
2025-08-13 11:10:47 -0600

    tercer commit

--- Actividad 16.py
+++ Actividad 16.py
@@ -8,3 +8,19 @@ class Libro:
 
     def mostrar_libro(self):
         return f"Codigo: {self.Codigo} - titulo: {self.titulo} - autor: {self.autor} - año Publicación: {self.año_Publicacion}"
+
+class manipulacion:
+    def __init__(self):
+        self.libros = []
+
+    def agregar_libro(self):
+        try:
+            Codigo = int(input("Ingrese código de la libro: "))
+            titulo = input("Ingrese titulo de la libro: ")
+            autor = input("Ingrese autor de la libro: ")
+            publicacion = input("Ingrese el año de la publicación: ")
+            self.libros.append(Libro(titulo, autor, Codigo))
+            print("Libros agregados\n")
+        except ValueError:
+            print("Error: el código debe ser números")
+
